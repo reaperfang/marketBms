33065fd0e8275abbac914ed8a343ecee218f5252
user <user@admin.com>
2020-02-14 17:49:27 +0800

    code

--- src/views/goods/classify.vue
+++ src/views/goods/classify.vue
@@ -275,18 +275,19 @@ export default {
             }
             this.confirm({title: '立即删除', icon: true, text: '删除后此分类无法展示，确认删除吗？'}).then(() => {
                 this._apis.goods.deleteCategory({id: node.data.id}).then((res) => {
-                    this.$notify({
-                                title: '成功',
-                                message: '删除成功！',
-                                type: 'success'
-                            });
-                    this.getTreeList()
                     if(res && res.msg == 'existProduct') {
                         this.confirm({title: '转移商品', icon: false, text: '是否将此分类其下的全部商品转移到其他分类中？'}).then(() => {
                             this.currentData = node.data.id
                             this.currentDialog = 'TransferGoodsDialog'
                             this.dialogVisible = true
                         })
+                    } else {
+                        this.$notify({
+                            title: '成功',
+                            message: '删除成功！',
+                            type: 'success'
+                        });
+                        this.getTreeList()
                     }
                 }).catch(error => {
                     this.$notify.error({
