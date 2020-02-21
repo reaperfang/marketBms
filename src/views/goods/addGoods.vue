284ae6791a820783f571bdccf8927733ee9cfd03
woke <like_0701@163.com>
2020-02-21 20:41:35 +0800

    商品修改

--- src/views/goods/addGoods.vue
+++ src/views/goods/addGoods.vue
@@ -1255,9 +1255,9 @@ export default {
             })
         },
         deleteSpec(index) {
-            //this.ruleForm.goodsInfos.splice(index, 1)
+            this.ruleForm.goodsInfos.splice(index, 1)
             this.deleteSpecArr.push(index)
-            this.addStyle()
+            //this.addStyle()
         },
         emptySpec(index) {
             this.ruleForm.goodsInfos.splice(index, 1, Object.assign({}, this.ruleForm.goodsInfos[index], {
