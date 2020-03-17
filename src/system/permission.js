20abc2f3cf716cbd93ec74f15e594a7055a9e9d1
fangyuan <fangyuan@300.cn>
2020-03-18 01:42:02 +0800

    框架优化,说明： 1.excel文件夹移入assets/js/中了 2.整理了main.js文件，分门别类 3.保超封装的全局confirm文件放到utils里面了，依然在main.js里引入，保持原有方法使用 4.以前src目录下的orderMixin.js文件不属于全局mixin,迁移到订单模块里了 5.路由优化，解决了控制台大量路由信息警告的问题 6.移除了无用的index.css和index.min.css文件 7.utils工具箱模块进行了拆分，拆分出了日期时间工具箱、数据转换工具箱、加解密安全工具箱 8.移除了部分无用的utils里的插件 9.原utils里的scroll-to和print插件迁移到了assets/js/中 10.店铺模块中的shopStyle.scss样式表迁移到styles目录下
    如果大家遇到任何异常请联系我
    
    Signed-off-by: fangyuan <fangyuan@300.cn>

--- src/system/permission.js
+++ src/system/permission.js
@@ -34,7 +34,7 @@ router.beforeEach((to, from, next) => {
         if(flag == 0){
           store.dispatch('GenerateRoutes', msfList).then(() => { // 根据roles权限生成可访问的路由表
             if(store.getters.addRouters.length != 0){
-              router.addRoutes(store.getters.addRouters) // 动态添加可访问路由表
+              router.selfAddRoutes(store.getters.addRouters) // 动态添加可访问路由表
               next({ ...to, replace: true }) // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
             }else{
               next({ path: '/401'})
---
+++ src/utils/security.js
@@ -0,0 +1,70 @@
+/* 安全相关工具，比如加解密转码等 */
+
+
+//对字符串进行加密(偏移加密)     
+export function compileStr1(code) {
+  var c = String.fromCharCode(code.charCodeAt(0) + code.length);
+  for (var i = 1; i < code.length; i++) {
+    c += String.fromCharCode(code.charCodeAt(i) + code.charCodeAt(i - 1));
+  }
+  return escape(c);
+}
+
+//对字符串进行解密(偏移解密)     
+export function uncompileStr1(code) {
+  code = unescape(code);
+  var c = String.fromCharCode(code.charCodeAt(0) - code.length);
+  for (var i = 1; i < code.length; i++) {
+    c += String.fromCharCode(code.charCodeAt(i) - c.charCodeAt(i - 1));
+  }
+  return c;
+}
+//对字符串进行加密(无偏移)     
+export function compileStr(code) {
+  return escape(code);
+}
+
+//对字符串进行解密(无偏移)     
+export function uncompileStr(code) {
+  return unescape(code);
+}
+
+/**
+ * AES加密
+ *
+ * @export
+ * @param {*} string
+ * @returns
+ */
+export function aesEncryption(key= '', string=''){
+      var key = CryptoJS.enc.Utf8.parse(key);
+      // ECB加密
+      var options={
+          mode: CryptoJS.mode.ECB,
+          padding: CryptoJS.pad.Pkcs7
+      }
+      var encryptedData = CryptoJS.AES.encrypt(string, key, options);
+      var encryptedBase64Str = encryptedData.toString();
+      return encryptedBase64Str;
+}
+
+/**
+ * AES解密
+ *
+ * @export
+ * @param {*} string
+ * @returns
+ */
+export function aesDecryption(key= '', string=''){
+    var encryptedBase64Str = string;
+    var options = {
+      mode: CryptoJS.mode.ECB,
+      padding: CryptoJS.pad.Pkcs7
+    };
+    var key = CryptoJS.enc.Utf8.parse(key);
+    // 解密
+    var decryptedData = CryptoJS.AES.decrypt(encryptedBase64Str, key, options);
+    // 解密后，需要按照Utf8的方式将明文转位字符串
+    var decryptedStr = decryptedData.toString(CryptoJS.enc.Utf8);
+    return decryptedStr;
+}
