6897479c4093b5766740bb023aed04f46b779e4d
chenbaochao <chenbaochao@300.cn>
2020-03-31 16:15:12 +0800

    code

--- src/views/goods/goodsList.vue
+++ src/views/goods/goodsList.vue
@@ -70,6 +70,11 @@
                         width="55">
                     </el-table-column>
                     <el-table-column
+                    prop="code"
+                    label="SPU编码"
+                    width="124">
+                    </el-table-column>
+                    <el-table-column
                     prop="name"
                     label="商品名称"
                     width="380">
