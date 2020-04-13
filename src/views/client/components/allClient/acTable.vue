a64e059efc928f366f5ab2e44f5df3830644b5d3
chenbaochao <chenbaochao@300.cn>
2020-04-13 17:24:37 +0800

    code

--- src/views/client/components/allClient/acTable.vue
+++ src/views/client/components/allClient/acTable.vue
@@ -34,19 +34,19 @@
           </div>
         </template>
       </el-table-column>
-      <el-table-column label="余额" sortable prop="balance">
+      <el-table-column label="余额" sortable="custom" prop="balance">
         <template slot-scope="scope">
           ¥{{scope.row.balance}}
         </template>
       </el-table-column>
-      <el-table-column prop="score" label="积分" sortable></el-table-column>
-      <el-table-column label="累计消费金额" sortable prop="totalDealMoney">
+      <el-table-column prop="score" label="积分" sortable="custom"></el-table-column>
+      <el-table-column label="累计消费金额" sortable="custom" prop="totalDealMoney">
         <template slot-scope="scope">
           ¥{{scope.row.totalDealMoney}}
         </template>
       </el-table-column>
-      <el-table-column prop="dealTimes" label="购买次数" sortable></el-table-column>
-      <el-table-column label="客单价（元）" sortable prop="perUnitPrice">
+      <el-table-column prop="dealTimes" label="购买次数" sortable="custom"></el-table-column>
+      <el-table-column label="客单价（元）" prop="perUnitPrice" sortable="custom">
         <template slot-scope="scope">
           ¥{{scope.row.perUnitPrice}}
         </template>
