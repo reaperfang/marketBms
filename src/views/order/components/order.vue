a6b4d7f5f792fa90f4e51095b9d071135e79431a
chenbaochao <chenbaochao@300.cn>
2020-04-14 17:04:56 +0800

    code

--- src/views/order/components/order.vue
+++ src/views/order/components/order.vue
@@ -156,7 +156,7 @@ export default {
                     return '特权价'
                 case 4:
                     return '赠品订单'
-                case 4:
+                case 5:
                     return '分佣订单'
             }
         },
