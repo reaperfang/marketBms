d4565f6662d0a6ce21d2eda013503b0257c679fc
李可 <like@300.cn>
2020-04-17 15:24:52 +0800

    改1.3样式bug

--- src/views/client/dialogs/clientInfo/issueCouponDialog.vue
+++ src/views/client/dialogs/clientInfo/issueCouponDialog.vue
@@ -11,7 +11,7 @@
           <div class="sel_cont" v-for="(i,index) in selectedCoupons" :key="index">
             <span class="sel_cont_name">{{i.name}}</span>
             <el-input-number v-model="i.frozenNum" :max="10" @change="(e) => changeNum(e, index)"></el-input-number>
-            <span class="addMainColor pointer" @click="handleDelete(index)" style="margin-left: 20px">删除</span>
+            <span class="addMainColor pointer" @click="handleDelete(index)" style="margin-left: 40px">删除</span>
           </div>
           <span class="add pointer" @click="handleAdd">添加</span>
         </div>
@@ -240,12 +240,16 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+/deep/ .el-table .cell{
+  padding-left: 16px;
+}
 /deep/ .el-input-number--small .el-input-number__decrease, .el-input-number--small .el-input-number__increase{
   background-color: #655EFF;
   color: #fff;
 }
 /deep/ .el-input-number--small{
-  width: 110px;
+  width: 60px;
+  padding-left: 10px;
 }
 /deep/ .el-dialog__header{
     background: #f1f0ff;
@@ -260,6 +264,7 @@ export default {
     border-radius: 50%;
     line-height: 18px;
     margin-top: 5px;
+    margin-left: -12px;
 }
 /deep/ .el-input-number--small .el-input-number__increase{
     width: 18px;
@@ -270,14 +275,12 @@ export default {
     border-radius: 50%;
     line-height: 18px;
     margin-top: 5px;
-}
-/deep/ .el-input-number--small{
-    width: 94px;
+    margin-right: -24px;
 }
 /deep/ .el-input-number--small .el-input__inner{
-    padding-left: 21px;
-    padding-right: 21px;
-    border: 0;
+    padding-left: 10px;
+    padding-right: 10px;
+    border: 1px solid #ddd;
     background: none;
 }
 /deep/ .el-dialog{
