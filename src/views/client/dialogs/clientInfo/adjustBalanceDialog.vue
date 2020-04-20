06fb12ec463c08f1d0f46f86fc2f604f8ab19c07
李可 <like@300.cn>
2020-04-20 15:13:43 +0800

    加余额输入限制

--- src/views/client/dialogs/clientInfo/adjustBalanceDialog.vue
+++ src/views/client/dialogs/clientInfo/adjustBalanceDialog.vue
@@ -42,6 +42,7 @@ export default {
   },
   methods: {
     number(event,val,ele) {
+      val = val.replace(/[^\.\d]/g,'');
       val = val.replace('-','');
       val = val.replace(/^0/g,'');
       this[ele] = val;
