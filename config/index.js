5cb42d2ed3d4f8d007285d1f23705e3f65a9fdd6
chenwanlin <chenwanlin@300.cn>
2020-05-09 15:15:07 +0800

    修改dev_v1.4环境变量

--- config/index.js
+++ config/index.js
@@ -5,148 +5,177 @@
 const path = require('path')
 
 module.exports = {
-  dev: {
+	dev: {
 
-    // Paths
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
-    host: '0.0.0.0', // can be overwritten by process.env.HOST
-    // host: '127.0.0.1', // can be overwritten by process.env.HOST
-    port: 9001, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
-    errorOverlay: true,
-    notifyOnErrors: false,
-    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
-    useEslint: false,
-    // If true, eslint errors and warnings will also be shown in the error overlay
-    // in the browser.
-    showEslintErrorsInOverlay: true,
+		// Paths
+		assetsSubDirectory: 'static',
+		assetsPublicPath: '/',
+		host: '0.0.0.0', // can be overwritten by process.env.HOST
+		// host: '127.0.0.1', // can be overwritten by process.env.HOST
+		port: 9001, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+		autoOpenBrowser: false,
+		errorOverlay: true,
+		notifyOnErrors: false,
+		poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+		useEslint: false,
+		// If true, eslint errors and warnings will also be shown in the error overlay
+		// in the browser.
+		showEslintErrorsInOverlay: true,
 
-    /**
-     * Source Maps
-     */
+		/**
+		 * Source Maps
+		 */
 
-    // https://webpack.js.org/configuration/devtool/#development
-    devtool: 'cheap-module-eval-source-map',
-    cssSourceMap: true,
+		// https://webpack.js.org/configuration/devtool/#development
+		devtool: 'cheap-module-eval-source-map',
+		cssSourceMap: true,
 
-    proxyTable: {
-      '/data-server/**': {
-        target: 'https://test-omo.aiyouyi.cn',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/data-server': ''
-        }
-      },
+		proxyTable: {
+			'/data-server/**': {
+				target: 'https://test-omo.aiyouyi.cn',
+				changeOrigin: true,
+				pathRewrite: {
+					'^/data-server': ''
+				}
+			},
 
-      /* 营销服务 */
-      '/sale_server/**': {
-        target: 'https://test-omo.aiyouyi.cn/mkt-api/',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/sale_server': ''
-        }
-      },
-      '/upload_server/**': {
-        target: 'https://test-omo.aiyouyi.cn',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/upload_server': ''
-        }
-      },
-    }
-  },
+			/* 营销服务 */
+			'/sale_server/**': {
+				target: 'https://test-omo.aiyouyi.cn/mkt-api/',
+				changeOrigin: true,
+				pathRewrite: {
+					'^/sale_server': ''
+				}
+			},
+			'/upload_server/**': {
+				target: 'https://test-omo.aiyouyi.cn',
+				changeOrigin: true,
+				pathRewrite: {
+					'^/upload_server': ''
+				}
+			},
+		}
+	},
 
-  test: {
-    // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+	test: {
+		// Template for index.html
+		index: path.resolve(__dirname, '../dist/index.html'),
 
-    // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+		// Paths
+		assetsRoot: path.resolve(__dirname, '../dist'),
+		assetsSubDirectory: 'static',
+		assetsPublicPath: './',
 
-    /**
-     * Source Maps
-     */
+		/**
+		 * Source Maps
+		 */
 
-    productionSourceMap: true,
-    // https://webpack.js.org/configuration/devtool/#production
-    devtool: 'cheap-module-eval-source-map',
+		productionSourceMap: true,
+		// https://webpack.js.org/configuration/devtool/#production
+		devtool: 'cheap-module-eval-source-map',
 
-    // Gzip off by default as many popular static hosts such as
-    // Surge or Netlify already gzip all static assets for you.
-    // Before setting to `true`, make sure to:
-    // npm install --save-dev compression-webpack-plugin
-    productionGzip: true,
-    productionGzipExtensions: ['js', 'css'],
+		// Gzip off by default as many popular static hosts such as
+		// Surge or Netlify already gzip all static assets for you.
+		// Before setting to `true`, make sure to:
+		// npm install --save-dev compression-webpack-plugin
+		productionGzip: true,
+		productionGzipExtensions: ['js', 'css'],
 
-    // Run the build command with an extra argument to
-    // View the bundle analyzer report after build finishes:
-    // `npm run build --report`
-    // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
-  },
+		// Run the build command with an extra argument to
+		// View the bundle analyzer report after build finishes:
+		// `npm run build --report`
+		// Set to `true` or `false` to always turn it on or off
+		bundleAnalyzerReport: process.env.npm_config_report
+	},
+	test3: {
+		// Template for index.html
+		index: path.resolve(__dirname, '../dist/index.html'),
 
-  pre: {
-    // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+		// Paths
+		assetsRoot: path.resolve(__dirname, '../dist'),
+		assetsSubDirectory: 'static',
+		assetsPublicPath: './',
 
-    // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+		/**
+		 * Source Maps
+		 */
 
-    /**
-     * Source Maps
-     */
+		productionSourceMap: true,
+		// https://webpack.js.org/configuration/devtool/#production
+		devtool: 'cheap-module-eval-source-map',
 
-    productionSourceMap: true,
-    // https://webpack.js.org/configuration/devtool/#production
-    devtool: 'cheap-module-eval-source-map',
+		// Gzip off by default as many popular static hosts such as
+		// Surge or Netlify already gzip all static assets for you.
+		// Before setting to `true`, make sure to:
+		// npm install --save-dev compression-webpack-plugin
+		productionGzip: true,
+		productionGzipExtensions: ['js', 'css'],
 
-    // Gzip off by default as many popular static hosts such as
-    // Surge or Netlify already gzip all static assets for you.
-    // Before setting to `true`, make sure to:
-    // npm install --save-dev compression-webpack-plugin
-    productionGzip: true,
-    productionGzipExtensions: ['js', 'css'],
+		// Run the build command with an extra argument to
+		// View the bundle analyzer report after build finishes:
+		// `npm run build --report`
+		// Set to `true` or `false` to always turn it on or off
+		bundleAnalyzerReport: process.env.npm_config_report
+	},
+	pre: {
+		// Template for index.html
+		index: path.resolve(__dirname, '../dist/index.html'),
 
-    // Run the build command with an extra argument to
-    // View the bundle analyzer report after build finishes:
-    // `npm run build --report`
-    // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
-  },
+		// Paths
+		assetsRoot: path.resolve(__dirname, '../dist'),
+		assetsSubDirectory: 'static',
+		assetsPublicPath: './',
 
-  build: {
-    // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+		/**
+		 * Source Maps
+		 */
 
-    // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+		productionSourceMap: true,
+		// https://webpack.js.org/configuration/devtool/#production
+		devtool: 'cheap-module-eval-source-map',
 
-    /**
-     * Source Maps
-     */
+		// Gzip off by default as many popular static hosts such as
+		// Surge or Netlify already gzip all static assets for you.
+		// Before setting to `true`, make sure to:
+		// npm install --save-dev compression-webpack-plugin
+		productionGzip: true,
+		productionGzipExtensions: ['js', 'css'],
 
-    productionSourceMap: false,
-    // https://webpack.js.org/configuration/devtool/#production
-    devtool: '#source-map',
+		// Run the build command with an extra argument to
+		// View the bundle analyzer report after build finishes:
+		// `npm run build --report`
+		// Set to `true` or `false` to always turn it on or off
+		bundleAnalyzerReport: process.env.npm_config_report
+	},
 
-    // Gzip off by default as many popular static hosts such as
-    // Surge or Netlify already gzip all static assets for you.
-    // Before setting to `true`, make sure to:
-    // npm install --save-dev compression-webpack-plugin
-    productionGzip: true,
-    productionGzipExtensions: ['js', 'css'],
+	build: {
+		// Template for index.html
+		index: path.resolve(__dirname, '../dist/index.html'),
 
-    // Run the build command with an extra argument to
-    // View the bundle analyzer report after build finishes:
-    // `npm run build --report`
-    // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
-  }
+		// Paths
+		assetsRoot: path.resolve(__dirname, '../dist'),
+		assetsSubDirectory: 'static',
+		assetsPublicPath: './',
+
+		/**
+		 * Source Maps
+		 */
+
+		productionSourceMap: false,
+		// https://webpack.js.org/configuration/devtool/#production
+		devtool: '#source-map',
+
+		// Gzip off by default as many popular static hosts such as
+		// Surge or Netlify already gzip all static assets for you.
+		// Before setting to `true`, make sure to:
+		// npm install --save-dev compression-webpack-plugin
+		productionGzip: true,
+		productionGzipExtensions: ['js', 'css'],
+
+		// Run the build command with an extra argument to
+		// View the bundle analyzer report after build finishes:
+		// `npm run build --report`
+		// Set to `true` or `false` to always turn it on or off
+		bundleAnalyzerReport: process.env.npm_config_report
+	}
 }
---
+++ config/test3.env.js
@@ -0,0 +1,9 @@
+module.exports = {
+	NODE_ENV: '"testing3"',
+  	ENV_CONFIG: '"test3"',
+  	DATA_API: '"http://test3-omo.aiyouyi.cn"',
+	SALE_API: '"/mkt-api"',
+	WEBSOCKET_server:'"ws://127.0.0.1:9000"',
+	staticHost: 'https://test3-m-aiyouyi.yun300.cn/',
+	UPLOAD_SERVER:'"http://test3-omo.aiyouyi.cn"',
+}
