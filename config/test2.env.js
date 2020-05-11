b80f1ae2076fccaecad4159624e88533bed17e29
wangyangyang <wangyangyang@staff.hexun.com>
2020-05-11 16:26:32 +0800

    test2 test3 环境变量配置

--- config/test2.env.js
+++ config/test2.env.js
@@ -5,10 +5,10 @@ const prodEnv = require('./production.env')
 module.exports = merge(prodEnv, {
 	NODE_ENV: '"test2"',
   	ENV_CONFIG: '"test2"',
-  	DATA_API: '"http://test2-omo.aiyouyi.cn"',
+  	DATA_API: '"//test2-omo.aiyouyi.cn"',
 	SALE_API: '"/mkt-api"',
 	WEBSOCKET_server:'"ws://127.0.0.1:9000"',
-	staticHost: 'https://test2-m-aiyouyi.yun300.cn/',
-	UPLOAD_SERVER:'"http://test2-omo.aiyouyi.cn"',
+	staticHost: '//test2-m-aiyouyi.yun300.cn/',
+	UPLOAD_SERVER:'"//test2-omo.aiyouyi.cn"',
 	APPLY: '"//test2-omo.aiyouyi.cn"'
 })
