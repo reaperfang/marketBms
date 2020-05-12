8a5bfca391de8a851733e34309d7f067cfa695f1
呼斌彬 <hubinbin@300.cn>
2020-05-12 10:52:44 +0800

    change key for qq map key

--- src/system/appConfig.js
+++ src/system/appConfig.js
@@ -9,7 +9,7 @@ export default {
 	//地图参数配置
 	map: {
 		url: 'https://map.qq.com/api/js?v=2.exp',
-		key: '7FYBZ-SKMKW-D5HR6-RUHRK-Z76EE-DNBYL',
+		key: 'C4XBZ-P2HWS-5BGOP-64Z2O-FROKK-JEFA7',
 		defaultCenter: [39.9046900000,116.4071700000],
 		apiBaseUrl: 'https://apis.map.qq.com/ws/'
 	},
