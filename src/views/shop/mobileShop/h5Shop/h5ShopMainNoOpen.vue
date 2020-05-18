7cd36b8041da10f4eb85fc2bd1306ca3a0998505
fangyuan <fangyuan@300.cn>
2020-05-18 16:11:27 +0800

    添加中转页面

---
+++ src/views/shop/mobileShop/h5Shop/h5ShopMainNoOpen.vue
@@ -0,0 +1,18 @@
+/* 移动H5未开通h5店铺 */
+<template>
+  <div class="app-container">
+    <div class="inner">
+      <i></i>
+      <p>您尚未开启移动H5店铺，请您去“应用中心-渠道应用”设置域名并开启店铺：</p>
+      <el-button>渠道应用</el-button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "h5ShopMainNoOpen"
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
