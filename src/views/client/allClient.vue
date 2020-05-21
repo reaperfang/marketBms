97d6ab6dde3fd27554d60b2bd3b1978c8b1a3eee
李可 <like@300.cn>
2020-05-21 10:51:30 +0800

    改样式及bug

--- src/views/client/allClient.vue
+++ src/views/client/allClient.vue
@@ -443,16 +443,18 @@ export default {
             if(channelNames.length > 0) {
                 if(channelNames.indexOf('不限') !== -1) {
                     channelIds = [];
-                } 
-                this.channelsList.map((item) => {
-                    channelNames.map((v) => {
-                        if(v == item.channerlName) {
-                            channelIds.push(item.id);
-                        }
-                    })
-                });
+                }else{
+                    this.channelsList.map((item) => {
+                        channelNames.map((v) => {
+                            if(v == item.channerlName) {
+                                channelIds.push(item.id);
+                            }
+                        })
+                    });
+                    channelIds = channelIds.join(',');
+                }
             }
-            channelIds = channelIds.join(',');
+            
             oForm.memberLabels = labelIds;
             oForm.channelIds = channelIds;
             if(oForm.memberType.length > 0) {
@@ -548,7 +550,7 @@ export default {
             }
             .down_img{
                 position: absolute;
-                left: 562px;
+                left: 580px;
                 top: 5px;
             }
             .more{
