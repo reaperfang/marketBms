c1170c3fd4e93a7a8b42370d333af9eff07da41a
fangyuan <fangyuan@300.cn>
2020-06-01 13:39:11 +0800

    修改h5店铺跳转地址和二维码地址
    Signed-off-by: fangyuan <fangyuan@300.cn>

--- src/views/shop/mobileShop/h5Shop/h5ShopMainDecorated.vue
+++ src/views/shop/mobileShop/h5Shop/h5ShopMainDecorated.vue
@@ -32,9 +32,9 @@
           </div>
           <div class="url">
             <h3>H5店铺预览地址：</h3>
-            <el-input v-model="shareUrl"></el-input>
+            <el-input v-model="h5ShareUrl"></el-input>
             <div class="url_btn">
-              <el-button type="primary" v-clipboard:copy="shareUrl" v-clipboard:success="onCopy">一键复制</el-button>
+              <el-button type="primary" v-clipboard:copy="h5ShareUrl" v-clipboard:success="onCopy">一键复制</el-button>
             </div>
           </div>
           <div class="tools">
@@ -76,20 +76,22 @@ export default {
   data() {
     return {
       loading: true,
-      webPageStatus: 0  //未绑定过域名
+      webPageStatus: 0,  //未绑定过域名
+      h5ShareUrl: ''  //h5店铺分享地址
     }
   },
   watch: {
-    decoratePageData: {
-      handler(newValue) {
-        this.shareUrl = newValue ? newValue.shareUrl : '';
-      },
-      deep: true
-    }
+    // decoratePageData: {
+    //   handler(newValue) {
+    //     this.shareUrl = newValue ? newValue.shareUrl : '';
+    //   },
+    //   deep: true
+    // }
   },
   created() {
-    this.shareUrl = this.decoratePageData ? this.decoratePageData.shareUrl : '';
+    // this.shareUrl = this.decoratePageData ? this.decoratePageData.shareUrl : '';
     this.getH5StoreStatus();
+    this.getH5StoreDomainList();
   },
   methods: {
     ...mapMutations(['SETCURRENT']),
@@ -122,7 +124,35 @@ export default {
     linkToBindDomain() {
       this.$router.push({path:'/apply',query:{paths:'/application/channelapp/mobileHFive', applyId:3}})
       this.SETCURRENT(8)
-    }
+    },
+
+    /* 获取H5店铺域名列表 */
+    getH5StoreDomainList() {
+      this.loading = true;
+      this._apis.shop.getH5StoreDomainList({status: 1}).then((response)=>{
+        this.h5ShareUrl = Array.isArray(response) && response[0] && response[0].domainName ? location.protocol + '//' + response[0].domainName : '';
+        this.getQrcode();
+        this.loading = false;
+      }).catch((error)=>{
+        console.error(error);
+        this.loading = false;
+      });
+    },
+
+      /* 获取二维码 */
+    getQrcode(codeType, callback) {
+      this._apis.shop.getQrcode({
+        url: this.h5ShareUrl,
+        width: '250',
+        height: '250',
+        logoUrl: this.shopInfo.logoCircle || this.shopInfo.logo
+      }).then((response)=>{
+        this.qrCode = `data:image/png;base64,${response}`;
+        callback && callback(response);
+      }).catch((error)=>{
+        console.error(error);
+      });
+    },
   }
 }
 </script>
