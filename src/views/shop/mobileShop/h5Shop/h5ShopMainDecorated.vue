9b7c699cb94bc5a4793291d40d7ace8d339c698d
fangyuan <fangyuan@300.cn>
2020-06-02 15:15:23 +0800

    修改移动h5入口显示逻辑bug
    Signed-off-by: fangyuan <fangyuan@300.cn>

--- src/views/shop/mobileShop/h5Shop/h5ShopMainDecorated.vue
+++ src/views/shop/mobileShop/h5Shop/h5ShopMainDecorated.vue
@@ -1,17 +1,17 @@
 /* 移动H5已装修 -- 继承自 微信已装修 */
 <template>
   <div class="shop_index_wrapper" v-loading="loading" v-calcHeight="160">
-    <div v-if="!webPageStatus || (webPageStatus === 0) || (webPageStatus === 2)" class="no_open">
+    <div v-if="!webStatus || (webStatus === 0) || (webStatus === 2)" class="no_open">
       <img src="../../../../assets/images/shop/no-open-h5.png" alt="">
       <p>您尚未开启移动H5店铺，请您去“应用中心-渠道应用”设置域名并开启店铺：</p>
       <div class="button" @click="linkToOpenH5"><span>渠道应用</span></div>
     </div>
-    <!-- <div v-if="webPageStatus === 2" class="no_bind">
+    <!-- <div v-if="webStatus === 2" class="no_bind">
       <img src="../../../../assets/images/shop/no-open-h5.png" alt="">
       <p>您尚未成功开启H5店铺，店铺域名未连接成功，请您去“渠道应用-H5店铺”重新设置：</p>
       <div class="button" @click="linkToBindDomain"><span>移动</span>H5</div>
     </div> -->
-    <template v-if="webPageStatus === 1">
+    <template v-if="webStatus === 1">
       <div class="tips">
         <i></i>
         <div class="words">
@@ -76,8 +76,16 @@ export default {
   data() {
     return {
       loading: true,
-      webPageStatus: 0,  //未绑定过域名
+      webStatus: null,  //未绑定过域名
       h5ShareUrl: ''  //h5店铺分享地址
+    }
+  },
+  props: {
+    webPageStatus: {
+      default: null
+    },
+    statusLoading: {
+      type: Boolean
     }
   },
   watch: {
@@ -87,10 +95,17 @@ export default {
     //   },
     //   deep: true
     // }
+    webPageStatus(newValue) {
+      this.webStatus = newValue;
+    },
+    statusLoading(newValue) {
+      this.loading = newValue;
+    }
   },
   created() {
     // this.shareUrl = this.decoratePageData ? this.decoratePageData.shareUrl : '';
-    this.getH5StoreStatus();
+    this.webStatus = this.webPageStatus;
+    this.loading = this.statusLoading;
     this.getH5StoreDomainList();
   },
   methods: {
@@ -99,18 +114,6 @@ export default {
       this.$message({
         message: `复制成功！`,
         type: 'success'
-      });
-    },
-
-    /* 获取H5店铺状态 */
-    getH5StoreStatus() {
-      this.loading = true;
-      this._apis.shop.getH5StoreStatus({}).then((response)=>{
-        this.webPageStatus = response.webPageStatus;
-        this.loading = false;
-      }).catch((error)=>{
-        console.error(error);
-        this.loading = false;
       });
     },
 
@@ -156,79 +159,83 @@ export default {
   }
 }
 </script>
-<style lang="scss" scoped>
-.no_open,.no_bind{
-  display:flex;
-  flex-direction: column;
-  align-items: center;
-  img{
-    margin-top:33px;
-    width:403px;
-    height:330px;
-  }
-  p{
-    font-size:16px;
-    font-family:MicrosoftYaHei;
-    color:rgba(68,67,75,1);
-    margin-top:37px;
-  }
-  .button{
-    margin-top: 24px;
-    width: 152px;
-    height: 34px;
-    /* line-height: 34px; */
-    background: #655EFF;
-    font-size: 14px;
-    font-family: PingFangSC-Regular,PingFang SC;
-    font-weight: 400;
-    color: #fff;
-    padding: 7px 20px 7px 32px;
-    box-sizing: border-box;
-    text-align: center;
-    border-radius: 4px;
-    cursor: pointer;
-    span{
-      letter-spacing: 8px;
+<style lang="scss">
+.shop_index_wrapper{
+
+  .no_open,.no_bind{
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    img{
+      margin-top:33px;
+      width:403px;
+      height:330px;
+    }
+    p{
+      font-size:16px;
+      font-family:MicrosoftYaHei;
+      color:rgba(68,67,75,1);
+      margin-top:37px;
+    }
+    .button{
+      margin-top: 24px;
+      width: 152px;
+      height: 34px;
+      /* line-height: 34px; */
+      background: #655EFF;
+      font-size: 14px;
+      font-family: PingFangSC-Regular,PingFang SC;
+      font-weight: 400;
+      color: #fff;
+      padding: 7px 20px 7px 32px;
+      box-sizing: border-box;
+      text-align: center;
+      border-radius: 4px;
+      line-height: 1.3;
+      cursor: pointer;
+      span{
+        letter-spacing: 8px;
+      }
     }
   }
-}
-.no_bind{
-  .button{
-    padding: 7px 30px 7px 32px;
+  .no_bind{
+    .button{
+      padding: 7px 30px 7px 32px;
+    }
   }
-}
-.tips{
-  display:flex;
-  justify-content: flex-start;
-  align-items: center;
-  margin-top:18px;
-  i{
-    background: url('../../../../assets/images/shop/abc.png') no-repeat 0 0;
-    width: 18px;
-    height: 20px;
-    display:block;
-    margin-right:14px;
-  }
-  .words{
-    font-size:16px;
-    font-family:MicrosoftYaHei;
-    color:rgba(101,94,255,1);
-  }
-}
-.url{
-  background: #fff;
-  padding: 25px 20px 15px 20px;
-  margin-top: 10px;
-  h3{
-    font-size:18px;
-    font-family:MicrosoftYaHei;
-    color:rgba(68,67,75,1);
-    margin-bottom:23px;
-  }
-  .url_btn{
-    margin-top: 23px;
+  .tips{
     display:flex;
-    justify-content: flex-end;
+    justify-content: flex-start;
+    align-items: center;
+    margin-top:18px;
+    i{
+      background: url('../../../../assets/images/shop/abc.png') no-repeat 0 0;
+      width: 18px;
+      height: 20px;
+      display:block;
+      margin-right:14px;
+    }
+    .words{
+      font-size:16px;
+      font-family:MicrosoftYaHei;
+      color:rgba(101,94,255,1);
+    }
+  }
+  .url{
+    background: #fff;
+    padding: 25px 20px 15px 20px;
+    margin-top: 10px;
+    h3{
+      font-size:18px;
+      font-family:MicrosoftYaHei;
+      color:rgba(68,67,75,1);
+      margin-bottom:23px;
+    }
+    .url_btn{
+      margin-top: 23px;
+      display:flex;
+      justify-content: flex-end;
+    }
   }
 }
 </style>
