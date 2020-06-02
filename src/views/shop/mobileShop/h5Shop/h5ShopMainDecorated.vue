ca15b955d7394ce84cd98ede52d5f4d2c4060375
fangyuan <fangyuan@300.cn>
2020-06-02 09:27:12 +0800

    修改移动h5店铺空白页逻辑

--- src/views/shop/mobileShop/h5Shop/h5ShopMainDecorated.vue
+++ src/views/shop/mobileShop/h5Shop/h5ShopMainDecorated.vue
@@ -1,16 +1,16 @@
 /* 移动H5已装修 -- 继承自 微信已装修 */
 <template>
   <div class="shop_index_wrapper" v-loading="loading" v-calcHeight="160">
-    <div v-if="!webPageStatus || (webPageStatus === 0)" class="no_open">
+    <div v-if="!webPageStatus || (webPageStatus === 0) || (webPageStatus === 2)" class="no_open">
       <img src="../../../../assets/images/shop/no-open-h5.png" alt="">
       <p>您尚未开启移动H5店铺，请您去“应用中心-渠道应用”设置域名并开启店铺：</p>
       <div class="button" @click="linkToOpenH5"><span>渠道应用</span></div>
     </div>
-    <div v-if="webPageStatus === 2" class="no_bind">
+    <!-- <div v-if="webPageStatus === 2" class="no_bind">
       <img src="../../../../assets/images/shop/no-open-h5.png" alt="">
       <p>您尚未成功开启H5店铺，店铺域名未连接成功，请您去“渠道应用-H5店铺”重新设置：</p>
       <div class="button" @click="linkToBindDomain"><span>移动</span>H5</div>
-    </div>
+    </div> -->
     <template v-if="webPageStatus === 1">
       <div class="tips">
         <i></i>
