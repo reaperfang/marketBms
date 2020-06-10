051b9910c49bceae78a313023c5db4aff2772131
chenbaochao <chenbaochao@300.cn>
2020-06-10 12:48:52 +0800

    code

--- src/views/goods/goodsListEmpty.vue
+++ src/views/goods/goodsListEmpty.vue
@@ -1,12 +1,5 @@
 <template>
     <div style="min-height: 100vh;">
-        <!-- <div class="goods-list-empty">
-            <div class="goods-list-empty-content">
-                <div class="image"></div>
-                <p>当前店铺没有商品，点击“新建商品”快去发布您的商品吧！</p>
-                <el-button @click="$router.push('/goods/addGoods')" class="add-goods" type="primary">新建商品</el-button>
-            </div>
-        </div> -->
         <div class="goods-list-empty">
             <div v-if="!loading" class="goods-list-empty-content">
                 <div class="image"></div>
