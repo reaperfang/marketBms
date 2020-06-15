e934bdbdffd8dd22a807152f19de6b57f782a010
董泳霆 <dongyongting@300.cn>
2020-06-15 15:53:53 +0800

    加console

--- config/env.dev.js
+++ config/env.dev.js
@@ -15,8 +15,8 @@ module.exports = {
   // Paths
   assetsSubDirectory: 'static',
   assetsPublicPath: '/',
-  host: '0.0.0.0', 
-  // host: '127.0.0.1', 
+  // host: '0.0.0.0', 
+  host: '127.0.0.1', 
   port: 9001, 
   autoOpenBrowser: false,
   errorOverlay: true,
