fb04dd1607f4659b722489a2a86108df4290feb4
wangjianyong <m13121950171@163.com>
2020-06-17 16:30:32 +0800

    模板信息置空适配

--- src/views/shop/mobileDecorate/template/templateEdit.vue
+++ src/views/shop/mobileDecorate/template/templateEdit.vue
@@ -97,7 +97,7 @@ export default {
     getPageList() {
       this.loading = true;
       this._apis.goodsOperate.getPagesByTemplateId({pageTemplateId: this.id}).then((response)=>{
-      	if (response === null) {
+      	if (response === null || response === undefined || response === '' || response.length === 0 || !response) {
 			this.$store.commit("clearAllData");
 		}
         this.pageList = [response];
