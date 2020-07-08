3ce721f111af890849f165ffb9c40609213c8472
fangyuan <fangyuan@300.cn>
2020-07-08 23:33:49 +0800

    修改模板列表无法滚动问题
    Signed-off-by: fangyuan <fangyuan@300.cn>

--- src/router/shop.js
+++ src/router/shop.js
@@ -55,7 +55,7 @@ export default [
 				path: "m_templateManageIndex",
 				component: () => import("@/views/shop/mobileDecorate/template/templateManageIndex"),
 				name: "m_templateManageIndex",
-				meta: { title: "模板管理", noCache: true, tabTitle: '移动装修',navType:2, m_shopEditor: true }
+				meta: { title: "模板管理", noCache: true, tabTitle: '移动装修',navType:2}
 			},
 			{
 				path: "m_templateEdit",
