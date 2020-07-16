99702976176b6af09d9a8b1b96a9463dcdad7c81
dibaosong <dibaosong@300.cn>
2020-07-16 11:24:14 +0800

    彻底恢复原样测试打印机

--- src/views/order/printingElectronicForm.vue
+++ src/views/order/printingElectronicForm.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="printing-electronic-form">
-        <div class="printing-electronic-form-title">打印电子面单预览</div>
-        <div class="printing-electronic-form-content print-content">
-            <div style="width: 375px;min-height:700px; margin: 0 auto;position:relative;">
-                <div v-for="(item, index) in tableData" :class="`printing-electronic-form-item printing-electronic-form-item_${index}`" :key="index" style="position:relative;height: 700px;page-break-after:always;" v-html="item.content"></div>
+        <div class="title">打印电子面单预览</div>
+        <div class="content print-content">
+            <div style="width: 375px; margin: 0 auto;">
+                <div v-for="(item, index) in tableData" :class="`item item_${index}`" :key="index" style="page-break-after:always;" v-html="item.content"></div>
             </div>
         </div>
         <div class="tc" style="margin-top: 20px;">
@@ -39,12 +39,6 @@ export default {
                 var body = window.document.body.innerHTML; 
 
                 //2.要打印的部分（#print里面的内容就是要打印的内容）
-                const printStyle = `<style media="print">
-                    @page {
-                        size: auto;  /* auto is the initial value */
-                        margin: 3mm; /* this affects the margin in the printer settings */
-                    }
-                </style>`;
 
                 window.document.body.innerHTML = document.getElementsByClassName("print-content")[0].innerHTML; 
 
@@ -85,12 +79,18 @@ export default {
     .printing-electronic-form {
         background-color: #fff;
         padding: 20px;
-        .printing-electronic-form-title {
+        .title {
             font-size: 16px;
             margin-bottom: 20px;
         }
-        .printing-electronic-form-item {
-            display: block;
+        .content {
+            text-align: center;
+        }
+        .item {
+            display: none;
+            &.item_0 {
+                display: block;
+            }
         }
     }
 </style>
