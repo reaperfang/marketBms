3891d5134fdf9a36ee41cd38b7883e77dc1c0b96
zhongqi7777 <yanlibo@300.cn>
2020-08-12 11:21:28 +0800

    add pay file

--- src/views/dashboard/components/pay/index.vue
+++ src/views/dashboard/components/pay/index.vue
@@ -1,65 +1,73 @@
 <template>
-  <div class="pay">
-    <el-row class="row">
-      <el-col :span="8"><div class="grid-box"></div></el-col>
-      <el-col :span="8"><div class="grid-box"></div></el-col>
-      <el-col :span="8"><div class="grid-box"></div></el-col>
-    </el-row>
-    <el-row class="row">
-      <el-col :span="8"><div class="grid-box"></div></el-col>
-      <el-col :span="8"><div class="grid-box"></div></el-col>
-      <el-col :span="8"><div class="grid-box"></div></el-col>
-    </el-row>
-  </div>
+	<div class="pay">
+		<el-row class="row">
+			<el-col :span="8"><div class="grid-box"></div></el-col>
+			<el-col :span="8"><div class="grid-box"></div></el-col>
+			<el-col :span="8"><div class="grid-box"></div></el-col>
+		</el-row>
+		<el-row class="row">
+			<el-col :span="8"><div class="grid-box"></div></el-col>
+			<el-col :span="8"><div class="grid-box"></div></el-col>
+			<el-col :span="8"><div class="grid-box"></div></el-col>
+		</el-row>
+	</div>
 </template>
 
 <script>
 import { mapGetters, mapActions, mapState } from "vuex";
 
 export default {
-  watch: {
-    // flowData(val) {
-    // }
-  },
-  props: {
-    // data: {
-    //   type: Object,
-    //   default: false
-    // }
-  },
-  components: {},
-  data: function() {
-    return {};
-  },
-  computed: {
-    //...mapState([""])
-  },
-  mounted() {},
-  beforeCreate() {},
-  created() {},
-  beforeMount() {},
-  beforeUpdate() {},
-  updated() {},
-  beforeDestroy() {},
-  destroyed: function() {},
-  methods: {
-    //...mapActions([""]),
-  },
+	watch: {
+		// flowData(val) {
+		// }
+	},
+	props: {
+		// data: {
+		//   type: Object,
+		//   default: false
+		// }
+	},
+	components: {},
+	data: function() {
+		return {};
+	},
+	computed: {
+		//...mapState([""])
+	},
+	mounted() {},
+	beforeCreate() {},
+	created() {},
+	beforeMount() {},
+	beforeUpdate() {},
+	updated() {},
+	beforeDestroy() {},
+	destroyed: function() {},
+	methods: {
+		//...mapActions([""]),
+	}
 };
 </script>
 
 <style lang="scss">
 .pay {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  .row {
-    flex: 1;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	.row {
+		flex: 1;
+		margin: 10px 0 0 10px;
 
-    .grid-box{
-        background: red;
-        height: 100%;
-    }
-  }
+		.grid-box {
+			background: RGBA(4, 25, 50, 1);
+			height: 100%;
+			margin-right: 10px;
+			cursor: pointer;
+		}
+
+		.grid-box:hover {
+			background: rgba(71, 225, 255, 0.05);
+			border: 1px solid rgba(6, 201, 221, 1);
+		}
+	}
 }
 </style>
