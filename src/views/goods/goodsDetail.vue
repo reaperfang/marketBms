0ca9c341cff4cb977e3965a30d46541fc2428ff6
wangfang180626 <wangfang@300.cn>
2020-08-17 15:26:19 +0800

    优化商品类目名称回显

--- src/views/goods/goodsDetail.vue
+++ src/views/goods/goodsDetail.vue
@@ -1184,7 +1184,11 @@ export default {
             if(data&&data.child){
                 this.leimuMessage = false;
                 this.ruleForm.productCategoryInfoId=data.child.id;
-                this.goodCategoryNames = data.name + ' / '+data.child.categoryName;
+                if(data.child.name){
+                    this.goodCategoryNames = data.name + ' / '+data.child.name;
+                }else if(data.child.categoryName){
+                    this.goodCategoryNames = data.name + ' / '+data.child.categoryName;
+                }
             }
 
         },
@@ -2149,11 +2153,14 @@ export default {
         },
         getCategoryIds(arr, id) {
             try {
-                let parentId = this.flatCategoryList.find(val => val.id == id).parentId
-                arr.unshift(id)
-                if(parentId && parentId != 0) {
-                    this.getCategoryIds(arr, parentId)
+                 if(this.flatCategoryList.find(val => val.id == id)){
+                    let parentId = this.flatCategoryList.find(val => val.id == id).parentId
+                    arr.unshift(id)
+                    if(parentId && parentId != 0) {
+                     this.getCategoryIds(arr, parentId)
+                    }   
                 }
+
             } catch(e) {
                 console.error(e)
             }
