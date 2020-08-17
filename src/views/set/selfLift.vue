a01066ac2bf4c6e5f07e96701734c06fbb374e9f
呼斌彬 <hubinbin@300.cn>
2020-08-17 17:41:57 +0800

    图表变更

--- src/views/set/selfLift.vue
+++ src/views/set/selfLift.vue
@@ -441,7 +441,7 @@ export default {
     closeSelfLift() {
        this.confirm({
         title: "提示",
-        iconSuccess: true,
+        iconWarning: true,
         text: '关闭后用户下单时将不能再使用上门自提，您确定要关闭吗？',
         confirmText: '确定',
         showCancelButton: true
