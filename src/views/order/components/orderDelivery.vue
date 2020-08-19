acec5ab235f8fa6cfeff76cea9ebe820d0f3b5bb
chenbaochao <chenbaochao@300.cn>
2020-08-19 16:22:48 +0800

    code

--- src/views/order/components/orderDelivery.vue
+++ src/views/order/components/orderDelivery.vue
@@ -175,7 +175,7 @@
                                 <span v-permission="['订单', '发货管理', '订单发货', '发货']" v-if="!scope.row.isFillUp" @click="$router.push(`/order/deliverGoods?orderType=order&sendType=one&ids=${scope.row.orderId}&_ids=${scope.row.id}`)">发货</span>
                                 <span v-else @click="$router.push(`/order/supplementaryLogistics?ids=${scope.row.orderId}&_ids=${scope.row.id}`)">补填物流</span>
                             </template>
-                            <template v-if="scope.row.status == 5">
+                            <template v-if="scope.row.status == 5 && scope.row.deliveryWay == 4">
                                 <span @click="verificationHandler">核销验证</span>
                             </template>
                         </div>
