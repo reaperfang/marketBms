56a4cf1cac81335a14b32732c5c99acac1e01e6b
zengjingqiang <zengjingqiang@300.cn>
2020-08-20 13:45:30 +0800

    查看快递公司账号说明

--- src/views/set/newElectronicFaceSheet.vue
+++ src/views/set/newElectronicFaceSheet.vue
@@ -27,16 +27,52 @@
             </el-form-item>
             <el-form-item v-if="isShowPwa" label="快递公司账号" prop="expressCompanyAccount">
                 <el-input v-model="ruleForm.expressCompanyAccount" placeholder="请输入，不超过20个字符"></el-input>
-                <span v-popover:popover class="account-explain">查看账号说明</span>
+				<!--<el-popover
+					placement="bottom-start"
+					width="1000"
+					trigger="click">
+					<p>快递公司支持情况</p>
+					<div style="margin-top: 15px">
+						顺丰速运、EMS、宅急送、圆通速递、百世快递、中通快递、韵达速递、申通快递、德邦快递、优速快递、京东快递、信丰物流、安能快递、国通快递、天天快递、跨越速运、邮政快递包裹、中铁快运、邮政国内标快、远成快运、全一快递、速尔快递、品骏快递。
+					</div>
+					<p style="margin-top: 15px">快运公司支持情况</p>
+					<div style="margin-top: 15px">德邦快运、安能快运、京东快运、龙邦快运。</div>
+					<el-table :data="tableData">
+						<el-table-column width="200" property="name" label="账号类型"></el-table-column>
+						<el-table-column width="780" property="company" label="支持快递公司"></el-table-column>
+					</el-table>
+					<span slot="reference" class="account-explain">查看账号说明</span>
+				</el-popover>-->
+
                 <el-popover
-                    ref="popover"
-                    placement="right"
-                    width="400"
-                    v-model="popVisible"
-                    trigger="hover"
+                    placement="bottom-start"
+					width="1000"
+                    trigger="click"
                 >
-                    <p class="p_title">账号说明：</p>
-                    <p>快递公司账号即在已选择的快递公司申请的用户ID和密码。</p>
+                    <!--<p class="p_title">账号说明：</p>
+                    <p>快递公司账号即在已选择的快递公司申请的用户ID和密码。</p>-->
+
+						<p>快递公司支持情况</p>
+						<div style="margin-top: 15px">
+							顺丰速运、EMS、宅急送、圆通速递、百世快递、中通快递、韵达速递、申通快递、德邦快递、优速快递、京东快递、信丰物流、安能快递、国通快递、天天快递、跨越速运、邮政快递包裹、中铁快运、邮政国内标快、远成快运、全一快递、速尔快递、品骏快递。
+						</div>
+						<p style="margin-top: 15px">快运公司支持情况</p>
+						<div style="margin-top: 15px">德邦快运、安能快运、京东快运、龙邦快运。</div>
+						<el-table
+							:data="tableData">
+							<el-table-column
+								property="name"
+								label="账号类型"
+								width="180">
+							</el-table-column>
+							<el-table-column
+								property="company"
+								width="780"
+								label="支持快递公司">
+							</el-table-column>
+						</el-table>
+
+					<span slot="reference" class="account-explain">查看账号说明</span>
                 </el-popover>
             </el-form-item>
             <el-form-item v-if="isShowPwa" label="密码" prop="expressCompanyPassword">
@@ -143,7 +179,24 @@ export default {
                 value: 4
               },
             ],
-			isShowPwa: false
+			isShowPwa: false,
+			dialogVisible: false,
+			tableData: [{
+				name: '无需申请直接打单',
+				company: '顺丰（SF）、EMS（EMS）（仅支持广东省内发货）、宅急送（ZJS）、邮政快递包裹（YZPY）、中铁快运（ZTKY）、邮政国内标快（YZBK），全一快递（UAPEX）',
+			}, {
+				name: '月结账号直接打单',
+				company: '德邦（DBL）、EMS（EMS）',
+			}, {
+				name: '快递鸟后台申请账号',
+				company: '优速（UC）、韵达（YD）、圆通（YTO）、远成（YCWL）、安能（ANE）、百世快递（HTKY）',
+			}, {
+				name: '线下（网点）申请账号',
+				company: 'EMS（EMS）、中通（ZTO）、申通（STO）、德邦（DBL）、京东（JD）、信丰（XFEX）、国通（GTO）、天天快递（HHTT）、速尔快递（SURE）、品骏快递（PJ）',
+			}, {
+				name: '快运电子面单',
+				company: '京东快运（JDKY）,安能快运（ANEKY）,德邦快运（DBLKY），龙邦快运（LB）。',
+			}]
         }
     },
     created() {
@@ -153,6 +206,9 @@ export default {
       }
     },
     methods: {
+		handleClose() {
+			this.dialogVisible = false
+		},
     	// 查询快递公司列表
       getExpressCompanyList() {
         this._apis.order
