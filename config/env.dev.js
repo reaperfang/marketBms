2162ac9383a268c14f17d5786e482aeae9c6c726
guanjiaming <guanjiaming@300.cn>
2020-08-26 09:31:54 +0800

    修改1.8开发test环境

--- config/env.dev.js
+++ config/env.dev.js
@@ -9,11 +9,11 @@ module.exports = {
   // ZX_API: '"/zx_server"',
   // ZX_HELP: '"//test-www.300.cn"',//链接
 
-	NODE_ENV: '"test3"',
-	DATA_API: '"//test3-omo.aiyouyi.cn"',
-	SALE_API: '"//test3-omo.aiyouyi.cn/mkt-api"',
-	UPLOAD_SERVER: '"//test3-omo.aiyouyi.cn"',
-	ZX_API: '"//test3-omo.aiyouyi.cn/cms-api"',
+	NODE_ENV: '"test5"',
+	DATA_API: '"//test5-omo.aiyouyi.cn"',
+	SALE_API: '"//test5-omo.aiyouyi.cn/mkt-api"',
+	UPLOAD_SERVER: '"//test5-omo.aiyouyi.cn"',
+	ZX_API: '"//test5-omo.aiyouyi.cn/cms-api"',
 	ZX_HELP: '"//test-www.300.cn"',
 
 
