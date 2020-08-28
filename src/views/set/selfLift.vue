85771e383dee2384333240a6252cbf4d72b2780f
呼斌彬 <hubinbin@300.cn>
2020-08-28 10:15:50 +0800

    bug修复

--- src/views/set/selfLift.vue
+++ src/views/set/selfLift.vue
@@ -6,10 +6,11 @@
       <el-switch
         @change="handleSwitch"
         v-model="isOpen"
+        v-permission="['设置', '上门自提', '默认页面', '新建自提点/从地址库选择/开启/关闭']"
         active-color="#13ce66"
         inactive-color="#cacfcb">
       </el-switch>
-      <span>开启后，用户下单时可选择上门自提的配送方式。</span>
+      <span v-permission="['设置', '上门自提', '默认页面', '新建自提点/从地址库选择/开启/关闭']">开启后，用户下单时可选择上门自提的配送方式。</span>
     </div>
     <!-- 按钮区域 -->
     <div class="btn-area">
