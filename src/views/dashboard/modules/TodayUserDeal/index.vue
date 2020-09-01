a76a78a252a803a6c86315e3a727fc9c61dfe516
zhongqi7777 <yanlibo@300.cn>
2020-09-01 14:26:56 +0800

    modified:   src/views/dashboard/components/map/china/index.vue         modified:   src/views/dashboard/modules/TodayUserDeal/index.vue

--- src/views/dashboard/modules/TodayUserDeal/index.vue
+++ src/views/dashboard/modules/TodayUserDeal/index.vue
@@ -30,7 +30,7 @@
 		</el-row>
 
 		<el-row class="grid-map">
-			<chinamap :mapData="mapData"></chinamap>
+			<chinamap :mapData="mapData" ref="chart"></chinamap>
 		</el-row>
 	</div>
 </template>
@@ -44,7 +44,7 @@ import { mapGetters, mapActions, mapState } from "vuex";
 export default {
 	watch: {
 		"dashboard.chinamap"(val) {
-			this.mapData=val
+			this.$refs.chart.showChart(this.initMapData(this.mapData, val));
 		}
 	},
 	props: {
@@ -59,7 +59,43 @@ export default {
 			startVal: 0,
 			endVal: 1000000,
 			cid: JSON.parse(localStorage.getItem("shopInfos")).id,
-            mapData:[]
+			//mapData: [],
+			mapData: [
+				{ name: "湖北", value: 0 },
+				{ name: "广东", value: 0 },
+				{ name: "河南", value: 0 },
+				{ name: "浙江", value: 0 },
+				{ name: "湖南", value: 0 },
+				{ name: "安徽", value: 0 },
+				{ name: "江西", value: 0 },
+				{ name: "江苏", value: 0 },
+				{ name: "重庆", value: 0 },
+				{ name: "山东", value: 0 },
+				{ name: "四川", value: 0 },
+				{ name: "黑龙江", value: 0 },
+				{ name: "北京", value: 0 },
+				{ name: "上海", value: 0 },
+				{ name: "河北", value: 0 },
+				{ name: "福建", value: 0 },
+				{ name: "广西", value: 0 },
+				{ name: "陕西", value: 0 },
+				{ name: "云南", value: 0 },
+				{ name: "海南", value: 0 },
+				{ name: "贵州", value: 0 },
+				{ name: "山西", value: 0 },
+				{ name: "天津", value: 0 },
+				{ name: "辽宁", value: 0 },
+				{ name: "甘肃", value: 0 },
+				{ name: "吉林", value: 0 },
+				{ name: "新疆", value: 0 },
+				{ name: "内蒙古", value: 0 },
+				{ name: "宁夏", value: 0 },
+				{ name: "香港", value: 0 },
+				{ name: "台湾", value: 0 },
+				{ name: "青海", value: 0 },
+				{ name: "澳门", value: 0 },
+				{ name: "西藏", value: 0 }
+			]
 		};
 	},
 	computed: {
@@ -78,11 +114,34 @@ export default {
 	methods: {
 		...mapActions(["maplist"]),
 		async init() {
-			console.log("//////////////////");
-			let parames = { cid: this.cid};
-
+			let parames = { cid: this.cid };
 			let res = await this._apis.dashboard.realTimeUser(parames);
-            this.maplist(res.areaOrderNumList);
+			this.maplist(res.areaOrderNumList);
+		},
+
+		initMapData(oldData, newData) {
+			let result = [];
+
+			for (var j = 0; j < oldData.length; j++) {
+				let newItem = this.getValueByName(oldData[j].name, newData);
+				if (newItem) {
+					result.push({
+						name: oldData[j].name,
+						value: newItem.order_cquantity_area
+					});
+				} else {
+					result.push(oldData[j]);
+				}
+			}
+
+			return result;
+		},
+
+		getValueByName(key, data) {
+			let result = data.find(item => {
+				return item.area_name === key;
+			});
+			return result;
 		}
 	}
 };
