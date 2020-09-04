3e06e221846eeafd825508ab812830210fbef99c
李可 <like@300.cn>
2020-09-04 15:52:58 +0800

    ID1023420

--- src/views/client/components/clientLabel/clTable.vue
+++ src/views/client/components/clientLabel/clTable.vue
@@ -22,7 +22,13 @@
           <span class="edit_span" v-else style="color:#000">{{scope.row.labelContains}}</span>
         </template>
       </el-table-column>
-      <el-table-column prop="labelCondition" label="标签条件"></el-table-column>
+      <el-table-column label="标签条件">
+        <template slot-scope="scope">
+          <div>
+            {{scope.row.labelCondition?scope.row.labelCondition.substring(0,scope.row.labelCondition.lastIndexOf(',')):""}}
+          </div>
+        </template>
+      </el-table-column>
       <el-table-column prop="createTime" label="创建时间"></el-table-column>
       <el-table-column label="操作">
         <template slot-scope="scope">
