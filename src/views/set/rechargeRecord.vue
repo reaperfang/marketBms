b582705e5ab39f253d46312e358837e3b827c79b
dibaosong <dibaosong@300.cn>
2020-09-07 17:45:28 +0800

    充值样式调整

--- src/views/set/rechargeRecord.vue
+++ src/views/set/rechargeRecord.vue
@@ -1,45 +1,45 @@
 <template>
-	<div class="shopExpress shopInfoBox">
-		<!-- 充值记录 -->
-		<h1 class="top">充值</h1>
-		<div class="rechangeBox">
-			<div>
-				<span>账户余额（元）</span>
-				<p class="price">{{price}}</p>
-				<p class="price-tip">账户余额不足，不能发单，无法使用达达配送</p>
+	<div>
+		<div class="recharge recharge-pt0">
+			<!-- 充值记录 -->
+			<h1 class="top">充值</h1>
+			<div class="rechangeBox clearfix">
+				<div class="fl">
+					<div class="price-title">账户余额（元）</div>
+					<div class="price-info">
+						<span class="price">{{price}}</span>
+						<span class="price-tip">账户余额不足，不能发单，无法使用达达配送</span>
+					</div>
+				</div>
+				<div class="fr recharge-btn">
+					<el-button @click="onRecharge" type="" plain size="small">充值</el-button>
+				</div>
 			</div>
-			<i></i>
-			<el-button @click="onRecharge" type="" plain size="small">充值</el-button>
 		</div>
-
-		<el-table
-			ref="multipleTable"
-			v-loading="loading"
-			:data="dataList"
-			style="width: 100%"
-			:header-cell-style="{background:'rgba(208, 214, 228, .2)', color:'#44434B', fontSize: '14px', fontWeight: '500'}"
-			>
-			<el-table-column
-			type="selection"
-			width="55">
-			</el-table-column>
-			<el-table-column
-			prop="roleName"
-			label="充值对象">
-			</el-table-column>
-			<el-table-column
-			prop="roleDesc"
-			label="角色描述">
-			</el-table-column>
-			<el-table-column
-			prop="createUserName"
-			label="创建人">
-			</el-table-column>
-			<el-table-column
-			prop="createTime"
-			label="创建时间">
-			</el-table-column>
-		</el-table>
+		<div class="recharge recharge-con">
+			<el-table
+				ref="multipleTable"
+				v-loading="loading"
+				:data="dataList"
+				style="width: 100%"
+				:header-cell-style="{background:'rgba(208, 214, 228, .2)', color:'#44434B', fontSize: '14px', fontWeight: '500'}"
+				>
+				<el-table-column
+				prop="roleName"
+				label="充值对象">
+				</el-table-column>
+				<el-table-column
+				prop="roleDesc"
+				label="充值金额">
+				</el-table-column>
+				<el-table-column
+				width="195"
+				align="center"
+				prop="createTime"
+				label="充值时间">
+				</el-table-column>
+			</el-table>
+		</div>
 	</div>
 </template>
 <script>
@@ -92,68 +92,67 @@
 </script>
 
 <style lang="scss" scoped>
-	.shopInfoBox {
+	.recharge {
 		width: 100%;
-		height: 100%;
 		background: #fff;
 		padding: 20px;
+		border-radius: 4px;
 
 		.top {
+			height: 52px;
+			line-height: 52px;
 			font-size: 16px;
 			color: #3d434a;
-			padding: 15px 0;
-			border-bottom: 1px solid #cacfcb;
+			border-bottom: 1px solid #D9DEE9;
 		}
 
 		.rechangeBox {
-			display: flex;
-			align-items: center;
-			margin: 20px 0 0 25px;
-
-			div {
-				width: 200px;
-        		padding-right: 10px;
-			}
-
-			span {
+			height: 117px;
+			padding: 0 20px;
+			.price-title {
+				padding-top: 25px;
 				font-size: 16px;
 				font-weight: 400;
 				color: #a7abc3;
 				line-height: 22px;
 			}
-
-			i {
-				display: inline-block;
-				width: 1px;
-				height: 34px;
-				background: #D5D9D5;
-				margin-right: 30px;
-			}
-
 			.price {
 				font-size: 32px;
 				font-weight: 500;
 				color: #655eff;
 				line-height: 45px;
+				font-family: PingFangSC-Medium, PingFang SC;
 			}
 			.price-tip {
+				margin-left: 15px;
+				font-size: 12px;
 				color: #FD4C2B;
 			}
 		}
 
-		/deep/ .el-button--small {
-			width: 80px;
-			height: 34px;
-			padding: 0;
-			line-height: 34px;
-			;
-		}
+		.recharge-btn {
+			padding-top: 41px;
+			/deep/ .el-button--small {
+				width: 80px;
+				height: 34px;
+				padding: 0;
+				line-height: 34px;
+				border-radius: 4px;
+			}
 
-		/deep/ .el-button {
-			color: #655EFF;
-			font-size: 14px;
-			border: 1px solid #655EFF;
+			/deep/ .el-button {
+				color: #655EFF;
+				font-size: 14px;
+				border: 1px solid #655EFF;
+			}
 		}
+	}
+	.recharge-pt0 {
+		padding-top: 0;
+	}
+	.recharge-con {
+		margin-top: 20px;
+		padding-bottom: 35px;
 	}
 
 </style>
