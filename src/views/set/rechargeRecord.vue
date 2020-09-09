e261862f7eb3dacf4c5cd6a208e0cdf234592df2
dibaosong <dibaosong@300.cn>
2020-09-09 20:13:21 +0800

    充值

--- src/views/set/rechargeRecord.vue
+++ src/views/set/rechargeRecord.vue
@@ -48,7 +48,7 @@
 				:page-sizes="[5, 10, 20, 50, 100, 200, 500]"
 				:page-size="pageSize*1"
 				:total="total*1"
-				layout="sizes, prev, pager, next, jumper"
+				layout="prev, pager, next, sizes"
 				>
 				</el-pagination>
 			</div>
