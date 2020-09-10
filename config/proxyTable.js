953c5bf1d2e2ff4d2f0a3fea7b36125041b8133b
呼斌彬 <hubinbin@300.cn>
2020-09-10 11:04:04 +0800

    test->pre

--- config/proxyTable.js
+++ config/proxyTable.js
@@ -4,7 +4,7 @@
 
 module.exports = {
     '/data-server/**': {
-      target: 'http://test-omo.aiyouyi.cn',//https://test-omo.aiyouyi.cn
+      target: 'http://pre-omo.aiyouyi.cn',//https://test-omo.aiyouyi.cn
       // target: 'http://172.22.146.1:8082',// 梅志文
       // target: 'http://172.22.146.129:8082',// 李文飞
       changeOrigin: true,
@@ -15,7 +15,7 @@ module.exports = {
 
     /* 营销服务 */
     '/sale_server/**': {
-      target: 'https://test-omo.aiyouyi.cn/mkt-api/',
+      target: 'https://pre-omo.aiyouyi.cn/mkt-api/',
       changeOrigin: true,
       pathRewrite: {
         '^/sale_server': ''
