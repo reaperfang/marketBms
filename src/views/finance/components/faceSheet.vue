d5a0baa14ff5f31d9015d54af196b6af4024ce5d
sunbilian <sunbilian@300.cn>
2020-09-11 15:53:08 +0800

    修改物流对账数据绑定参数

--- src/views/finance/components/faceSheet.vue
+++ src/views/finance/components/faceSheet.vue
@@ -108,7 +108,7 @@
         </el-pagination>
       </div>
     </div>
-	  <exportTipDialog :data=currentData  :dialogVisible.sync="dialogVisible"></exportTipDialog>
+	  <exportTipDialog :data="currentData" :dialogVisible.sync="dialogVisible"></exportTipDialog>
   </div>
 </template>
 
