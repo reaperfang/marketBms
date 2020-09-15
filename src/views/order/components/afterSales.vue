0935b40035477b8b7948e02a0551c0be9b11cc02
wangfang180626 <wangfang@300.cn>
2020-09-15 10:18:17 +0800

    修改售后列表、发货列表、评价管理列表订单编号分行的bug

--- src/views/order/components/afterSales.vue
+++ src/views/order/components/afterSales.vue
@@ -535,7 +535,7 @@ export default {
     }
     /deep/ .el-table .cell {
         padding-left: 0;
-        padding-right: 20px;
+        padding-right: 10px;
     }
     /deep/ .input-with-select .el-input-group__prepend {
         background-color: #fff;
