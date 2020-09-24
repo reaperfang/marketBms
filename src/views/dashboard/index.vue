7759e1ab7c5947f6fd36b8e3a8fbc4e14f415659
zhongqi7777 <yanlibo@300.cn>
2020-09-24 18:59:05 +0800

    modified:   src/views/dashboard/index.vue

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -302,8 +302,21 @@ export default {
 	}
 
 	*::-webkit-scrollbar-track {
+		//::-webkit-scrollbar-track
 		/*滚动条内部轨道*/
 		background: #000b23;
 	}
+
+	*::-webkit-scrollbar-track-piece {
+		background: #000b23;
+	}
+
+	// 	::-webkit-scrollbar — 整个滚动条
+	// ::-webkit-scrollbar-track — 滚动条轨道
+	// ::-webkit-scrollbar-thumb — 滚动条上的滚动滑块
+	// ::-webkit-scrollbar-button — 滚动条上的按钮 (上下箭头)
+	// ::-webkit-scrollbar-track-piece — 滚动条没有滑块的轨道部分
+	// ::-webkit-scrollbar-corner — 边角，即当同时有垂直滚动条和水平滚动条时交汇的部分
+	// ::-webkit-resizer — 某些元素的corner部分的部分样式(例:textarea的可拖动按钮)
 }
 </style>
