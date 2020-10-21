4bdcf609e079fc6b28c414b09f57e21d83f8603e
zengjingqiang <zengjingqiang@300.cn>
2020-10-21 18:05:40 +0800

    数据大屏bug

--- src/views/dashboard/components/v-chart/ring/index.vue
+++ src/views/dashboard/components/v-chart/ring/index.vue
@@ -132,7 +132,7 @@ export default {
 	},
 	mounted() {
 		if (screen.width == 1440){
-			this.chartSettings.radius = [38, 48]
+			this.chartSettings.radius = [34, 44]
 		}else if (screen.width == 1600){
 			this.chartSettings.radius = [35, 45]
 		}else {
